a669922847ecf5c5ba774e5c010724cf0cd92037
Denis Volok <denis.volok@pm.me>
2022-11-03 10:57:23 +0400

    complete "transfer" methods implementation

--- contracts/NFT/NFToken.sol
+++ contracts/NFT/NFToken.sol
@@ -2,11 +2,19 @@
 
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "./IERC721.sol";
 import "./ERC165.sol";
+import "./IERC721Metadata.sol";
 
-contract NFToken is IERC721, ERC165 {
+contract NFToken is IERC721, ERC165, IERC721Metadata {
+    using Strings for uint256;
+
     bytes4 private constant _INTERFACE_ID_ERC721 = 0x80ac58cd;
+    string private _name;
+    string private _symbol;
+    string private _tokenBaseURI;
 
     mapping(uint256 => address) private _tokenToOwner;
     mapping(address => uint256) private _ownedTokensCount;
@@ -30,8 +38,15 @@ contract NFToken is IERC721, ERC165 {
         _;
     }
 
-    constructor() {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory tokenBaseURI_
+    ) {
         _registerInterface(_INTERFACE_ID_ERC721);
+        _name = name_;
+        _symbol = symbol_;
+        _tokenBaseURI = tokenBaseURI_;
     }
 
     function balanceOf(address _owner) external view returns (uint256) {
@@ -47,16 +62,19 @@ contract NFToken is IERC721, ERC165 {
         address _from,
         address _to,
         uint256 _tokenId,
-        bytes calldata data
-    ) external isValidNFToken(_tokenId) authorizeTokenTransfer(_tokenId) {
+        bytes memory _data
+    ) public {
         transferFrom(_from, _to, _tokenId);
+        require(_checkOnERC721Received(_from, _to, _tokenId, _data));
     }
 
     function safeTransferFrom(
         address _from,
         address _to,
         uint256 _tokenId
-    ) external {}
+    ) external {
+        safeTransferFrom(_from, _to, _tokenId, "");
+    }
 
     function transferFrom(
         address _from,
@@ -69,7 +87,7 @@ contract NFToken is IERC721, ERC165 {
         _tokenToOwner[_tokenId] = _to;
         _ownedTokensCount[_from] -= 1;
         _ownedTokensCount[_to] += 1;
-        
+
         if (_tokenToApproval[_tokenId] != address(0)) {
             _tokenToApproval[_tokenId] == address(0);
         }
@@ -84,4 +102,39 @@ contract NFToken is IERC721, ERC165 {
     function getApproved(uint256 _tokenId) external view returns (address) {}
 
     function isApprovedForAll(address _owner, address _operator) external view returns (bool) {}
+
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function _checkOnERC721Received(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes memory _data
+    ) private returns (bool) {
+        if (_to.code.length == 0) {
+            return true;
+        }
+
+        require(
+            IERC721Receiver(_to).onERC721Received(msg.sender, _from, _tokenId, _data) ==
+                IERC721Receiver.onERC721Received.selector
+        );
+        
+        return true;
+    }
+
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    function tokenURI(uint256 _tokenId)
+        external
+        view
+        isValidNFToken(_tokenId)
+        returns (string memory)
+    {
+        return string(abi.encodePacked(_tokenBaseURI, _tokenId.toString()));
+    }
 }
